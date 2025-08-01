41c97ce23b92f3acf6ac2f4810f7597ca0d2fdc9
unknown <clappertontee@gmail.com>
2025-08-01 20:12:35 +0200

    I have get information from modal using their IDs

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-import {initialTasks}from './initialData.js'
+import {initialTasks} from './initialData.js'
+
+// *****getting hold of the modal part through their IDs*******
+
+let selectedTask = null;
+const modal = document.getElementById("modal");
+const taskInput = document.getElementById("taskTitleInput");
+const taskDiscriptionInput = document.getElementById('taskDescriptionInput');
+const closeModalBtn = document.getElementById("close-modal");
 
 
